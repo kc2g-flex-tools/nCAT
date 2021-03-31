f4122e01ed4def8de1f854d4302c987b56acfffb
Andrew Rodland <andrew@cleverdomain.org>
2021-03-31 16:25:27 -0400

    Wire up the get/set RIT/XIT commands

--- freq.go
+++ freq.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math"
 	"strconv"
+
+	"github.com/kc2g-flex-tools/flexclient"
 )
 
 func init() {
@@ -20,6 +22,38 @@ func init() {
 		names{{`F`}, {`\set_freq`}},
 		NewHandler(
 			set_freq,
+			Args(1),
+		),
+	)
+
+	hamlib.AddHandler(
+		names{{`j`}, {`\get_rit`}},
+		NewHandler(
+			get_ritxit("rit"),
+			Args(0),
+		),
+	)
+
+	hamlib.AddHandler(
+		names{{`J`}, {`\set_rit`}},
+		NewHandler(
+			set_ritxit("rit"),
+			Args(1),
+		),
+	)
+
+	hamlib.AddHandler(
+		names{{`z`}, {`\get_xit`}},
+		NewHandler(
+			get_ritxit("xit"),
+			Args(0),
+		),
+	)
+
+	hamlib.AddHandler(
+		names{{`Z`}, {`\set_xit`}},
+		NewHandler(
+			set_ritxit("xit"),
 			Args(1),
 		),
 	)
@@ -51,4 +85,55 @@ func set_freq(_ Conn, args []string) (string, error) {
 	}
 
 	return Success, nil
+}
+
+func get_ritxit(which string) func(Conn, []string) (string, error) {
+	return func(_ Conn, _ []string) (string, error) {
+		slice, ok := fc.GetObject("slice " + SliceIdx)
+
+		if !ok {
+			return "", fmt.Errorf("couldn't get slice %s", SliceIdx)
+		}
+
+		on, err := strconv.Atoi(slice[which+"_on"])
+		if err != nil {
+			return "", err
+		}
+
+		freq, err := strconv.Atoi(slice[which+"_freq"])
+		if err != nil {
+			return "", err
+		}
+
+		if on == 0 {
+			freq = 0
+		}
+
+		return fmt.Sprintf("%d\n", freq), nil
+	}
+}
+
+func set_ritxit(which string) func(Conn, []string) (string, error) {
+	return func(_ Conn, args []string) (string, error) {
+		freq, err := strconv.Atoi(args[0])
+		if err != nil {
+			return "", err
+		}
+
+		obj := flexclient.Object{}
+		if freq == 0 {
+			obj[which+"_on"] = "0"
+			obj[which+"_freq"] = "0"
+		} else {
+			obj[which+"_on"] = "1"
+			obj[which+"_freq"] = fmt.Sprintf("%d", freq)
+		}
+
+		res := fc.SliceSet(SliceIdx, obj)
+		if res.Error != 0 {
+			return "", fmt.Errorf("slice set %08X", res.Error)
+		}
+
+		return Success, nil
+	}
 }
