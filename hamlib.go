kc2g-flex-tools/nCAT

commit 8a047d3aebce48978bc6ca02519075da4cb7117a

big command-handling refactor
Make handlers aware of their names (formerly names were only passed to
AddHandler).

Promote the "long name" to a first class citizen -- every handler has
one long name and zero or one short names, instead of an array of names.

The backslash is no longer part of the long name, it's parsed out separately.

All of this will, with a little more annotating, allow supporting the
hamlib "extended protocol", in which the first thing a command has to
send back in a response is its own long name (whether invoked via the
long or short name).

hamlib.go
@@ -33,15 +33,28 @@ func CustomError(err error, response string) error {
 type HandlerFunc func(context.Context, []string) (string, error)
 
 type Handler struct {
-	cb          HandlerFunc
-	minArgs     *int
-	maxArgs     *int
-	allArgs     bool
-	errResponse *string
+	Name         string
+	ShortName    string
+	cb           HandlerFunc
+	requiredArgs []string
+	minArgs      *int
+	maxArgs      *int
+	allArgs      bool
+	errResponse  *string
 }
 
 type Option interface {
 	apply(h *Handler)
+}
+
+type requiredArgs []string
+
+func (ra requiredArgs) apply(h *Handler) {
+	x := []string(ra)
+	h.requiredArgs = x
+}
+func RequiredArgs(ra ...string) requiredArgs {
+	return requiredArgs(ra)
 }
 
 type MinArgs int
@@ -79,11 +92,11 @@ func (er ErrResponse) apply(h *Handler) {
 	h.errResponse = &x
 }
 
-type names [][]string
-
-func NewHandler(cb HandlerFunc, opts ...Option) Handler {
+func NewHandler(name, shortName string, cb HandlerFunc, opts ...Option) Handler {
 	h := Handler{
-		cb: cb,
+		Name:      name,
+		ShortName: shortName,
+		cb:        cb,
 	}
 
 	for _, o := range opts {
@@ -178,9 +191,10 @@ func (s *HamlibServer) handleCmd(ctx context.Context, conn *Conn, line string) b
 	}
 
 	cmd := line[0:1]
-	rest := strings.TrimLeft(line[1:], " ")
+	var rest string
 
 	if cmd == "\\" {
+		line = line[1:]
 		spaceIdx := strings.Index(line, " ")
 		if spaceIdx == -1 {
 			cmd = line
@@ -189,6 +203,9 @@ func (s *HamlibServer) handleCmd(ctx context.Context, conn *Conn, line string) b
 			cmd = line[:spaceIdx]
 			rest = line[spaceIdx+1:]
 		}
+	} else {
+		cmd = "short:" + cmd
+		rest = strings.TrimLeft(line[1:], " ")
 	}
 
 	parts := []string{cmd}
@@ -227,7 +244,8 @@ func (s *HamlibServer) handleCmd(ctx context.Context, conn *Conn, line string) b
 			} else if handler.maxArgs != nil && len(args) > *handler.maxArgs {
 				e = fmt.Errorf("required max %d args, got %d", *handler.maxArgs, len(args))
 			} else {
-				ret, e = handler.cb(ctx, args)
+				handlerCtx := context.WithValue(ctx, handlerKey{}, &handler)
+				ret, e = handler.cb(handlerCtx, args)
 			}
 
 			if e != nil {
@@ -255,19 +273,29 @@ func (s *HamlibServer) handleCmd(ctx context.Context, conn *Conn, line string) b
 	return false
 }
 
-func (s *HamlibServer) AddHandler(names names, handler Handler) {
+func (s *HamlibServer) AddHandler(handler Handler) {
 	s.Lock()
 	defer s.Unlock()
 
+	names := []string{handler.Name}
+	if handler.ShortName != "" {
+		names = append(names, "short:"+handler.ShortName)
+	}
+
 	for _, name := range names {
 		table := s.handlers
-		for _, part := range name[:len(name)-1] {
+		nameWithArgs := []string{name}
+		if len(handler.requiredArgs) > 0 {
+			nameWithArgs = append(nameWithArgs, handler.requiredArgs...)
+		}
+
+		for _, part := range nameWithArgs[:len(nameWithArgs)-1] {
 			if table[part] == nil {
 				table[part] = map[string]interface{}{}
 			}
 			table = table[part].(map[string]interface{})
 		}
 
-		table[name[len(name)-1]] = handler
+		table[nameWithArgs[len(nameWithArgs)-1]] = handler
 	}
 }

audio.go
@@ -10,17 +10,19 @@ import (
 
 func init() {
 	hamlib.AddHandler(
-		names{{`l`, `AF`}, {`\get_level`, `AF`}},
 		NewHandler(
+			"get_level", "l",
 			get_level_af,
+			RequiredArgs("AF"),
 			Args(0),
 		),
 	)
 
 	hamlib.AddHandler(
-		names{{`L`, `AF`}, {`\set_level`, `AF`}},
 		NewHandler(
+			"set_level", "L",
 			set_level_af,
+			RequiredArgs("AF"),
 			Args(1),
 		),
 	)

context.go
@@ -7,3 +7,9 @@ type connKey struct{}
 func getConn(ctx context.Context) *Conn {
 	return ctx.Value(connKey{}).(*Conn)
 }
+
+type handlerKey struct{}
+
+func getHandler(ctx context.Context) *Handler {
+	return ctx.Value(handlerKey{}).(*Handler)
+}

dump_state.go
@@ -65,8 +65,8 @@ var protocol1StateString = "vfo_ops=0x0\n" +
 
 func init() {
 	hamlib.AddHandler(
-		names{{`\dump_state`}},
 		NewHandler(
+			"dump_state", "",
 			func(ctx context.Context, _ []string) (string, error) {
 				conn := getConn(ctx)
 				var levelGetCaps uint64

freq.go
@@ -11,8 +11,8 @@ import (
 
 func init() {
 	hamlib.AddHandler(
-		names{{`f`}, {`\get_freq`}},
 		NewHandler(
+			"get_freq", "f",
 			get_freq,
 			Args(0),
 			ErrResponse("ERR\n"),
@@ -20,40 +20,40 @@ func init() {
 	)
 
 	hamlib.AddHandler(
-		names{{`F`}, {`\set_freq`}},
 		NewHandler(
+			"set_freq", "F",
 			set_freq,
 			Args(1),
 		),
 	)
 
 	hamlib.AddHandler(
-		names{{`j`}, {`\get_rit`}},
 		NewHandler(
+			"get_rit", "j",
 			get_ritxit("rit"),
 			Args(0),
 		),
 	)
 
 	hamlib.AddHandler(
-		names{{`J`}, {`\set_rit`}},
 		NewHandler(
+			"set_rit", "J",
 			set_ritxit("rit"),
 			Args(1),
 		),
 	)
 
 	hamlib.AddHandler(
-		names{{`z`}, {`\get_xit`}},
 		NewHandler(
+			"get_xit", "z",
 			get_ritxit("xit"),
 			Args(0),
 		),
 	)
 
 	hamlib.AddHandler(
-		names{{`Z`}, {`\set_xit`}},
 		NewHandler(
+			"set_xit", "Z",
 			set_ritxit("xit"),
 			Args(1),
 		),

metering.go
@@ -34,13 +34,15 @@ var hamlibToFlex = map[string]string{}
 var pktReader bytes.Reader
 
 func init() {
-	handler := NewHandler(get_level_metering,
-		AllArgs(true),
-		Args(2),
-	)
-
 	for meter := range meters {
-		hamlib.AddHandler(names{{`l`, meter}, {`\get_level`, meter}}, handler)
+		handler := NewHandler("get_level", "l",
+			get_level_metering,
+			RequiredArgs(meter),
+			AllArgs(true),
+			Args(2),
+		)
+
+		hamlib.AddHandler(handler)
 	}
 }
 

mode.go
@@ -10,8 +10,8 @@ import (
 
 func init() {
 	hamlib.AddHandler(
-		names{{`m`}, {`\get_mode`}},
 		NewHandler(
+			"get_mode", "m",
 			get_mode,
 			Args(0),
 			Args(0),
@@ -19,8 +19,8 @@ func init() {
 	)
 
 	hamlib.AddHandler(
-		names{{`M`}, {`\set_mode`}},
 		NewHandler(
+			"set_mode", "M",
 			set_mode,
 			Args(2),
 		),

powerstat.go
@@ -4,8 +4,8 @@ import "context"
 
 func init() {
 	hamlib.AddHandler(
-		names{{`\get_powerstat`}},
 		NewHandler(
+			"get_powerstat", "",
 			func(ctx context.Context, _ []string) (string, error) {
 				return "1\n", nil
 			},

commit ec2ee862624276d8cf91bb98023f57f047894494

Fix WRN No handler found cmd=["short:q"]
Broken by commit 8a047d3 (in release v0.3-20231121.1)

hamlib.go
@@ -240,7 +240,7 @@ func (s *HamlibServer) handleCmd(ctx context.Context, conn *Conn, line string) b
 	log.Trace().Strs("cmd", parts).Msg("")
 	s.RLock()
 	defer s.RUnlock()
-	if cmd == "q" {
+	if cmd == "short:q" || cmd == "short:Q" {
 		return true
 	}
 